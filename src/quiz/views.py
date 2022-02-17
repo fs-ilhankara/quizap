50854a5554451dc7dbf2c15f88fd34126a40f31b
mirackarakasli <ilhansamikara@gmail.com>
2022-02-17 21:39:26 +0300

    added auth.

--- src/quiz/views.py
+++ src/quiz/views.py
@@ -4,19 +4,19 @@ from rest_framework import generics
 from .models import Category, Question, Quiz 
 from .serializers import CategoryDetailSerializer, CategorySerializer, QuestionSerializer
 from .pagination import MyPagination
-#from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny
 #from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 
 class CategoryList(generics.ListAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
-    #permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated] #allowany de herkes ulaşabilir
     # authentication_classes = [TokenAuthentication, SessionAuthentication]
 
 
 class CategoryDetail(generics.ListAPIView):
     serializer_class = CategoryDetailSerializer
-    #permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated] #sadece giriş yapmış olanlar ulaşabilir
 
     def get_queryset(self):
         queryset = Quiz.objects.all()
@@ -28,7 +28,7 @@ class CategoryDetail(generics.ListAPIView):
 
 class QuizDetail(generics.ListAPIView):
     serializer_class = QuestionSerializer
-    #permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     pagination_class = MyPagination
     
 
